5a2aa39847d772afd1395fde623b32b4c77f14ac
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2024-08-31 10:50:25 -0300

    Exemplo de herança.

---
+++ exemplo_classes/src/br/com/impacta/oo/manipula/Manipulacao.java
@@ -0,0 +1,28 @@
+package br.com.impacta.oo.manipula;
+
+import br.com.impacta.oo.base.Animal;
+import br.com.impacta.oo.zoo.Cachorro;
+
+public class Manipulacao {
+
+	public static void main(String[] args) {
+		
+		Animal animal = new Animal();
+		
+		Cachorro cachorro = new Cachorro();
+		cachorro.setEspecie("Canídeo");
+		cachorro.setGenero('f');
+		cachorro.setPeso(5);
+		cachorro.setTamanho(50);
+		cachorro.setTipo("Shit'zu");
+		cachorro.setNome("Aríete");
+		
+		System.out.println("Espécie : " + cachorro.getEspecie());
+		System.out.println("Genêro : " + (cachorro.getGenero() == 'f' ? "Fêmea" : "Macho"));
+		System.out.println("Peso : " + cachorro.getPeso());
+		System.out.println("Tamanho : " + cachorro.getTamanho());
+		System.out.println("Raça : " + cachorro.getTipo());
+		System.out.println("Nome : " + cachorro.getNome());
+	}
+
+}
---
+++ exemplo_classes/src/br/com/impacta/oo/zoo/Cachorro.java
@@ -0,0 +1,21 @@
+package br.com.impacta.oo.zoo;
+
+import br.com.impacta.oo.base.Animal;
+
+public class Cachorro extends Animal{
+	
+	private String nome;
+	
+	public Cachorro() {}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+	
+}
